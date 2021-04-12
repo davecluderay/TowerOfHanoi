db3094c98a7bb0ea66152364ab3f3601a4c382e2
Dave Cluderay <daveclud@hotmail.com>
2021-04-12 20:27:43 +0100

    Add more detailed solver to assist with animation.

--- src/tower-of-hanoi/solver.test.ts
+++ src/tower-of-hanoi/solver.test.ts
@@ -1,6 +1,6 @@
-import { createSolution } from './solver';
+import { createDetailedSolution, createSolution } from './solver';
 
-test('produces correct solution for 2 discs', () => {
+test('basic solver produces correct solution for 2 discs', () => {
   const moves = createSolution(2);
   expect(moves).toEqual([
     { from: 1, to: 2 },
@@ -9,7 +9,7 @@ test('produces correct solution for 2 discs', () => {
   ]);
 });
 
-test('produces correct solution for 3 discs', () => {
+test('basic solver produces correct solution for 3 discs', () => {
   const moves = createSolution(3);
   expect(moves).toEqual([
     { from: 1, to: 3 },
@@ -22,7 +22,7 @@ test('produces correct solution for 3 discs', () => {
   ]);
 });
 
-describe('produces optimal solutions', () => {
+describe('basic solver produces optimal solutions', () => {
   test.each([
     [1, 1],
     [2, 3],
@@ -36,6 +36,46 @@ describe('produces optimal solutions', () => {
     [10, 1023],
   ])('given %d discs, produces %d moves', (discs: number, expectedMoveCount: number) => {
     const moves = createSolution(discs);
+    expect(moves).toHaveLength(expectedMoveCount);
+  });
+});
+
+test('detailed solver produces correct solution for 2 discs', () => {
+  const moves = createDetailedSolution(2);
+  expect(moves).toEqual([
+    { disc: 1, from: { base: 1, level: 2 }, to: { base: 2, level: 1 } },
+    { disc: 2, from: { base: 1, level: 1 }, to: { base: 3, level: 1 } },
+    { disc: 1, from: { base: 2, level: 1 }, to: { base: 3, level: 2 } },
+  ]);
+});
+
+test('detailed solver produces correct solution for 3 discs', () => {
+  const moves = createDetailedSolution(3);
+  expect(moves).toEqual([
+    { disc: 1, from: { base: 1, level: 3 }, to: { base: 3, level: 1 } },
+    { disc: 2, from: { base: 1, level: 2 }, to: { base: 2, level: 1 } },
+    { disc: 1, from: { base: 3, level: 1 }, to: { base: 2, level: 2 } },
+    { disc: 3, from: { base: 1, level: 1 }, to: { base: 3, level: 1 } },
+    { disc: 1, from: { base: 2, level: 2 }, to: { base: 1, level: 1 } },
+    { disc: 2, from: { base: 2, level: 1 }, to: { base: 3, level: 2 } },
+    { disc: 1, from: { base: 1, level: 1 }, to: { base: 3, level: 3 } },
+  ]);
+});
+
+describe('detailed solver produces optimal solutions', () => {
+  test.each([
+    [1, 1],
+    [2, 3],
+    [3, 7],
+    [4, 15],
+    [5, 31],
+    [6, 63],
+    [7, 127],
+    [8, 255],
+    [9, 511],
+    [10, 1023],
+  ])('given %d discs, produces %d moves', (discs: number, expectedMoveCount: number) => {
+    const moves = createDetailedSolution(discs);
     expect(moves).toHaveLength(expectedMoveCount);
   });
 });
--- src/tower-of-hanoi/solver.ts
+++ src/tower-of-hanoi/solver.ts
@@ -1,4 +1,34 @@
 type Move = { from: number; to: number };
+type DiscPosition = { base: number; level: number };
+type DetailedMove = { disc: number; from: DiscPosition; to: DiscPosition };
+
+export function createDetailedSolution(discCount: number): DetailedMove[] {
+  const runningCounts = [discCount, 0, 0];
+  function solve(discsToMove: number, from: number, to: number): DetailedMove[] {
+    if (discsToMove === 0) return [];
+
+    var other = 6 - from - to;
+
+    // Move all except the largest disc to the other base.
+    const before = solve(discsToMove - 1, from, other);
+
+    // Move the largest disc to the destination base.
+    runningCounts[from - 1] = runningCounts[from - 1] - 1;
+    runningCounts[to - 1] = runningCounts[to - 1] + 1;
+    const main = {
+      disc: discsToMove,
+      from: { base: from, level: runningCounts[from - 1] + 1 },
+      to: { base: to, level: runningCounts[to - 1] },
+    };
+
+    // Move all except the largest disc to the destination base.
+    const after = solve(discsToMove - 1, other, to);
+
+    return [...before, main, ...after];
+  }
+
+  return solve(discCount, 1, 3);
+}
 
 export function createSolution(discCount: number): Move[] {
   function solve(discsToMove: number, from: number, to: number): Move[] {
